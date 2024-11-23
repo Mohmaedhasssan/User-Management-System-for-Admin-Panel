e01c18240de0ab5b979361c4c0eaf34019d66b77
mohmead hasssan <mohamed.hassan.official55@gmail.com>
2024-11-23 23:47:41 +0200

    database sample created

---
+++ config/db.sample.php
@@ -0,0 +1,8 @@
+<?php
+// Sample database configuration file
+return [
+    'host' => 'localhost',          // Database host
+    'user' => 'your_username',      // Database username
+    'password' => 'your_password',  // Database password
+    'dbname' => 'your_database',    // Database name
+];
